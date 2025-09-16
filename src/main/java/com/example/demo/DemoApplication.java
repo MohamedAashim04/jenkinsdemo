903fe32b6fd3d9eebc087dd18e2f10865c290672
MohamedAashim04 <mohamedaashim03@gmail.com>
2025-09-16 17:02:23 +0530

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -15,7 +15,7 @@ public class DemoApplication {
 
     @GetMapping("/")
     public String home() {
-        return "Hello from Jenkins Pipeline!";
+        return "Hello from Jenkins Pipeline!welcome learn jenkins";
     }
 }
 
